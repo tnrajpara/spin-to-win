82aea957f511bd49e6d75c3fe00b1fdf490c81f6
tnrajpara <rajparatarang66@gmail.com>
2025-02-21 19:06:01 +0530

    fix window undefined error

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,20 @@
-import SpinnerWheel from './SpinnerWheel'
+'use client'
+import { lazy, Suspense, useEffect, useState } from "react";
+
+const SpinnerWheel = lazy(() => import("./SpinnerWheel"));
 
 export default function Home() {
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      setIsClient(true);
+    }
+  }, []);
+
   return (
-  
-    <>
-    <SpinnerWheel />
-    </>
+    <Suspense fallback={"Loading..."}>
+      {isClient ? <SpinnerWheel /> : null}
+    </Suspense>
   );
 }
